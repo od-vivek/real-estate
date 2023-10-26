b3c0cdb8419e563d85fd8c0381f2c511c469ed1b
od-vivek <odedravivek.10@gmail.com>
2023-10-26 16:41:04 +0530

    created a search route

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -82,3 +82,46 @@ exports.getListing = async (req, res, next) => {
         return next(error);
     }
 }
+
+exports.getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.offer;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: [false, true] }
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt'; //by default we search for latest one.
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            offer, furnished, parking, type
+        }).sort({ [sort]: order }).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error);
+    }
+}
--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -7,5 +7,6 @@ router.post('/create', verifyUser, listingController.createListing);
 router.delete('/delete/:id' , verifyUser , listingController.deleteListing);
 router.post('/update/:id' , verifyUser , listingController.updateListing);
 router.get('/get/:id' , listingController.getListing);
+router.get('/get' , listingController.getListings); //for search
 
 module.exports = router;
