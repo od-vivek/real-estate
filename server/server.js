9e0bd897f9994b9f8beedf50808524fc5496f04f
od-vivek <odedravivek.10@gmail.com>
2023-10-29 19:39:40 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
+const cors = require('cors');
 
 app.use(cors({
     origin:["http://localhost:3000", "https://gregarious-raindrop-091940.netlify.app"],
