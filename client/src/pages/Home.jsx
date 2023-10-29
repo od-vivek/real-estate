866cace643aee6a02a65d52778b69b734e5f3075
od-vivek <odedravivek.10@gmail.com>
2023-10-29 09:23:44 +0530

    saved changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,7 +47,7 @@ export default function Home() {
   }, []);
   return (
     <div>
-      {/* top */}
+      {/* top */ }
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
